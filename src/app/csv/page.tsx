4786fa6ec3355b09c38820d25cb206edeb0f9e44
henry <henrywlam@gmail.com>
2024-01-31 22:49:26 -0500

    Updated table format.

--- src/app/csv/page.tsx
+++ src/app/csv/page.tsx
@@ -10,6 +10,8 @@ import { CSVLink } from "react-csv";
 import Table from "./table";
 import { Encounter } from "./types";
 
+const API_URL = process.env.ENCOUNTER_API_URL || "http://127.0.0.1:5000/encounters";
+
 export default function Home() {
     const [file, setFile] = useState<File>();
     const [csvData, setCsvData] = useState<Encounter[]>();
@@ -27,15 +29,13 @@ export default function Home() {
         if (file) {
             filereader.onload = async (e) => {
                 const csvOutput = e.target?.result as string;
-                console.log(csvOutput);
                 const formData = new FormData();
                 formData.append("csv", csvOutput);
-                console.log(formData.get("csv"));
                 const options = {
                     method: "POST",
                     body: formData,
                 };
-                fetch("http://127.0.0.1:5000/encounters", options)
+                fetch(API_URL, options)
                     .then((response) => response.json())
                     .then((data: string) => {
                         csv.parse(
@@ -58,26 +58,47 @@ export default function Home() {
 
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
-            <h1> CSV Generator </h1>
-            <form>
+            <h1 className="text-center mb-4 text-4xl font-extrabold">
+                Hospital Encounter Generator
+            </h1>
+            <form className="justify-center">
+                <label
+                    className="text-center block mb-2 text-l font-medium text-gray-900"
+                    htmlFor="fileUpload"
+                >
+                    Upload Events CSV File
+                </label>
                 <input
+                    className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-200 dark:border-gray-300 dark:placeholder-gray-300"
+                    aria-describedby="file_input_help"
                     type="file"
                     id="fileUpload"
                     name="fileUpload"
                     accept=".csv"
                     onChange={handleOnChange}
                 />
-                <input
-                    type="submit"
-                    className="border-2 rounded px-2 py-1 bg-white"
-                    onClick={(e) => handleOnSubmit(e)}
-                />
+                <div className="flex justify-center">
+                    <input
+                        type="submit"
+                        className="block border border-gray-300 rounded px-2 py-1 bg-white my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        onClick={(e) => handleOnSubmit(e)}
+                        value={"Process Events"}
+                    />
+                </div>
             </form>
             {csvData ? (
-                <>
-                    <CSVLink data={csvData}>Download CSV</CSVLink>{" "}
+                <div className="flex flex-col justify-center mt-5">
+                    <div className="flex justify-center">
+                        <CSVLink
+                            className="mt-3 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                            data={csvData}
+                            filename="encounters.csv"
+                        >
+                            Download Encounters CSV
+                        </CSVLink>{" "}
+                    </div>
                     <Table csvData={csvData}></Table>
-                </>
+                </div>
             ) : null}
         </main>
     );
--- src/app/csv/table.tsx
+++ src/app/csv/table.tsx
@@ -5,13 +5,17 @@ import {
     useReactTable,
     flexRender,
     getPaginationRowModel,
+    getSortedRowModel,
+    SortingState,
 } from "@tanstack/react-table";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Encounter } from "./types";
 
 export default function Table(props: { csvData: Encounter[] }) {
     const { csvData } = props;
     const columnHelper = createColumnHelper<Encounter>();
+    const [sorting, setSorting] = useState<SortingState>([]);
+
     const columns = useMemo(
         () => [
             columnHelper.accessor("PatientIdentifier", {
@@ -55,24 +59,46 @@ export default function Table(props: { csvData: Encounter[] }) {
     const table = useReactTable({
         data: csvData,
         columns,
+        state: {
+            sorting,
+        },
+        onSortingChange: setSorting,
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
     });
 
     return (
-        <>
+        <div>
             <table className="border-separate border-spacing-0.5 border border-slate-500">
                 <thead className="">
                     {table.getHeaderGroups().map((headerGroup) => (
                         <tr key={headerGroup.id} className="bg-gray-200">
                             {headerGroup.headers.map((header) => (
                                 <th key={header.id} className="px-2">
-                                    {header.isPlaceholder
-                                        ? null
-                                        : flexRender(
-                                              header.column.columnDef.header,
-                                              header.getContext()
-                                          )}
+                                    {header.isPlaceholder ? null : (
+                                        <div
+                                            {...{
+                                                className:
+                                                    header.column.getCanSort()
+                                                        ? "cursor-pointer select-none"
+                                                        : "",
+                                                onClick:
+                                                    header.column.getToggleSortingHandler(),
+                                            }}
+                                        >
+                                            {flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                            {{
+                                                asc: " 🔼",
+                                                desc: " 🔽",
+                                            }[
+                                                header.column.getIsSorted() as string
+                                            ] ?? null}
+                                        </div>
+                                    )}
                                 </th>
                             ))}
                         </tr>
@@ -80,7 +106,10 @@ export default function Table(props: { csvData: Encounter[] }) {
                 </thead>
                 <tbody>
                     {table.getRowModel().rows.map((row) => (
-                        <tr key={row.id} className="even:bg-gray-200 odd:bg-white">
+                        <tr
+                            key={row.id}
+                            className="even:bg-gray-200 odd:bg-white"
+                        >
                             {row.getVisibleCells().map((cell) => (
                                 <td key={cell.id} className="px-2">
                                     {flexRender(
@@ -94,70 +123,75 @@ export default function Table(props: { csvData: Encounter[] }) {
                 </tbody>
             </table>
             <div className="h-2" />
-            <div className="flex items-center gap-2">
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.setPageIndex(0)}
-                    disabled={!table.getCanPreviousPage()}
-                >
-                    {"<<"}
-                </button>
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.previousPage()}
-                    disabled={!table.getCanPreviousPage()}
-                >
-                    {"<"}
-                </button>
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.nextPage()}
-                    disabled={!table.getCanNextPage()}
-                >
-                    {">"}
-                </button>
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-                    disabled={!table.getCanNextPage()}
-                >
-                    {">>"}
-                </button>
-                <span className="flex items-center gap-1">
-                    <div>Page</div>
-                    <strong>
-                        {table.getState().pagination.pageIndex + 1} of{" "}
-                        {table.getPageCount()}
-                    </strong>
-                </span>
-                <span className="flex items-center gap-1">
-                    | Go to page:
-                    <input
-                        type="number"
-                        defaultValue={table.getState().pagination.pageIndex + 1}
+            <div className="flex justify-center">
+                <div className="flex items-center gap-2">
+                    <button
+                        className="border rounded p-1"
+                        onClick={() => table.setPageIndex(0)}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        {"<<"}
+                    </button>
+                    <button
+                        className="border rounded p-1"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        {"<"}
+                    </button>
+                    <button
+                        className="border rounded p-1"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        {">"}
+                    </button>
+                    <button
+                        className="border rounded p-1"
+                        onClick={() =>
+                            table.setPageIndex(table.getPageCount() - 1)
+                        }
+                        disabled={!table.getCanNextPage()}
+                    >
+                        {">>"}
+                    </button>
+                    <span className="flex items-center gap-1">
+                        <div>Page</div>
+                        <strong>
+                            {table.getState().pagination.pageIndex + 1} of{" "}
+                            {table.getPageCount()}
+                        </strong>
+                    </span>
+                    <span className="flex items-center gap-1">
+                        | Go to page:
+                        <input
+                            type="number"
+                            defaultValue={
+                                table.getState().pagination.pageIndex + 1
+                            }
+                            onChange={(e) => {
+                                const page = e.target.value
+                                    ? Number(e.target.value) - 1
+                                    : 0;
+                                table.setPageIndex(page);
+                            }}
+                            className="border p-1 rounded w-16"
+                        />
+                    </span>
+                    <select
+                        value={table.getState().pagination.pageSize}
                         onChange={(e) => {
-                            const page = e.target.value
-                                ? Number(e.target.value) - 1
-                                : 0;
-                            table.setPageIndex(page);
+                            table.setPageSize(Number(e.target.value));
                         }}
-                        className="border p-1 rounded w-16"
-                    />
-                </span>
-                <select
-                    value={table.getState().pagination.pageSize}
-                    onChange={(e) => {
-                        table.setPageSize(Number(e.target.value));
-                    }}
-                >
-                    {[10, 20, 30, 40, 50].map((pageSize) => (
-                        <option key={pageSize} value={pageSize}>
-                            Show {pageSize}
-                        </option>
-                    ))}
-                </select>
+                    >
+                        {[10, 20, 30, 40, 50].map((pageSize) => (
+                            <option key={pageSize} value={pageSize}>
+                                Show {pageSize}
+                            </option>
+                        ))}
+                    </select>
+                </div>
             </div>
-            <div>{table.getRowModel().rows.length} Rows</div>
-        </>
+        </div>
     );
 }
