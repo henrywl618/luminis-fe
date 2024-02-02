fccfc248b97481f9cee4f6fbd92ef8a67d419f75
henry <henrywlam@gmail.com>
2024-02-01 19:06:05 -0500

    Fixed button background.

--- src/app/csv/page.tsx
+++ src/app/csv/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
                 <div className="flex justify-center">
                     <input
                         type="submit"
-                        className="block border border-gray-300 rounded px-2 py-1 bg-white my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        className="block border border-gray-300 rounded px-2 py-1 my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                         onClick={(e) => handleOnSubmit(e)}
                         value={"Process Events"}
                     />
